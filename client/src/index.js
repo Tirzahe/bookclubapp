c60b0fa9de311c173876ab08fd65b83b4f1da423
tirzahe <tirzah410@gmail.com>
2018-04-30 10:50:44 -0600

    update index file prepare for merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./Redux/redux-index";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App/App";
-
-ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')
-);
-
-import React from 'react';
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import store from "./redux";
